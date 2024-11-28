fb2d0f746a750b90a4091a7090d0c89f824d3bb5
OverlordBaconPants <bjmar867@gmail.com>
2024-11-28 16:25:36 -0500

    included /health route for checks for render

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from __init__ import create_app
 
 app = create_app()
 
+@app.route('/health')
+def health_check():
+    # Perform any necessary checks here
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(debug=True)
